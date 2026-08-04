66e7d18f9551110dabcb7554cd4577fd51631dde
Azimn <jratican@gmail.com>
2026-08-04 11:33:10 -0500

    Materialize canonical Peg Drop source from exact archive parts

--- pegdrop_source/materialize_source.sh
+++ pegdrop_source/materialize_source.sh
@@ -3,7 +3,7 @@ set -eu
 SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 DEST=${1:-"$SCRIPT_DIR/build"}
 mkdir -p "$DEST"
-tr -d '\r\n' < "$SCRIPT_DIR/canonical_source.tgz.b64" | base64 -d > "$DEST/canonical_source.tgz"
+cat "$SCRIPT_DIR"/archive_parts/part*.b64 | tr -d '\r\n' | base64 -d > "$DEST/canonical_source.tgz"
 echo '691f6e61a70bfe4fe4965edfa49a1b85d7d1db35d3d9215c5c75171fbd6c2485  '"$DEST"'/canonical_source.tgz' | sha256sum -c -
 tar -xzf "$DEST/canonical_source.tgz" -C "$DEST"
 rm "$DEST/canonical_source.tgz"
